960a94f4d719fb49760f9e8c5140c0d8f1915a73
Ronaldo Patterson <51467659+ronapatterson@users.noreply.github.com>
2026-04-22 19:28:37 +0000

    refactor(theme): drop inline brand tokens; stylesheet is source of truth

--- wordpress/AscendMen/wp-content/themes/kadence-child/functions.php
+++ wordpress/AscendMen/wp-content/themes/kadence-child/functions.php
@@ -16,21 +16,6 @@ add_action( 'wp_enqueue_scripts', function() {
         [],
         null
     );
-
-    // Inline brand tokens so they are always present in <head>,
-    // independent of external stylesheet caching.
-    $am_brand_tokens = ':root{'
-        . '--am-flame-blue:#29ABE2;'
-        . '--am-navy:#1B2A4A;'
-        . '--am-dark-nav:#111d33;'
-        . '--am-steel-blue:#4A7FC1;'
-        . '--am-white:#FFFFFF;'
-        . '--am-font-heading:"Oswald","Arial Narrow",sans-serif;'
-        . '--am-font-body:"Inter",system-ui,-apple-system,sans-serif;'
-        . '--am-container:1200px;'
-        . '--am-section-pad:80px;'
-        . '}';
-    wp_add_inline_style( 'kadence-child-style', $am_brand_tokens );
 });
 
 require_once get_stylesheet_directory() . '/includes/programs-cpt.php';
